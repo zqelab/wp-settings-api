c668ba468b7dad8c6cfb24268e085b784a071346
ZQE <dev@zqe.io>
2021-11-10 01:16:40 +0600

    update

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Wp Settings API
+ * Plugin URI: https://github.com/zqelab/wp-settings-api
+ * Description: WordPress Settings API Testing
+ * Author: ZQE
+ * Author URI: https://zqe.io
+ * Version: 1.0
+ */
